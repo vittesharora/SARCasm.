eec47c5c045ad97e5eda7ecc6c9859680eddd967
achinbpl <achinparashar1000@gmail.com>
2019-06-23 12:47:45 +0530

    levels + user-model added

--- SARCasm/users/models.py
+++ SARCasm/users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from game.models import Level
+from django.dispatch import reciever
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+class Player(models.Model):
+	user = models.OneToOneField(User, on_delete = models.CASCADE)
+	current_level = models.ForeignKey(Level, default = Level.DEFAULT_LEVEL)
+	current_level_time = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return self.user.username
+	
+	def get_level(self):
+		return self.current_level.level_id
+
+	def get_name(self):
+		return self.user.first_name + " " + self.user.last_name
+
+# @reciever(post_save, sender=User)
+# def update_user_profile(sender, instance, created, **kwargs):
+# 	if created:
+# 		Player.objects.create(user=instance)
+# 	instance.player.save()
