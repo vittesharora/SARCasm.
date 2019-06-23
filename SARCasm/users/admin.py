f58cdaaf463fb3892ec114137c11645b98a5311c
achinbpl <achinparashar1000@gmail.com>
2019-06-24 02:49:08 +0530

    register + player done

--- SARCasm/users/admin.py
+++ SARCasm/users/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Player
 # Register your models here.
 class PlayerAdmin(admin.ModelAdmin):
-	list_display = ('user', 'current_level')
+	list_display = ('user', 'current_level', 'current_level_time')
 
 admin.site.register(Player, PlayerAdmin)
